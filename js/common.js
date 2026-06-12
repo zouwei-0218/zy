9989cae10287f43c0feca50c63547576e21bc927
zou_wei <215889806@qq.com>
2026-06-12 18:32:02 +0800

    更改whatsapp短连接

--- js/common.js
+++ js/common.js
@@ -58,7 +58,7 @@ function initNavbar() {
  * 处理点击WhatsApp按钮后的跳转
  */
 function toWhatsApp(){
-    window.open('https://wa.me/message/5Z5VATVJXTD2C1', '_blank')
+    window.open('https://wa.me/message/NLQDVBCLKV7WF1', '_blank')
 }
 
 
@@ -466,7 +466,7 @@ function initGlobalModal() {
                                     <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                                         <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"/>
                                     </svg>
-                                    <span>+86 18923123804</span>
+                                    <span>+86 13543653945</span>
                                 </div>
                                 <div class="modal-contact-item">
                                     <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
